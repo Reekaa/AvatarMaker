4d126ffb5d35d33a745d09c4a6ff19e464dd5c82
Reka F <reka.forgacs@lloydsbanking.com>
2020-02-29 22:17:05 +0000

    added my face

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div class='main'>
+        <div class='backdrop'></div>
+
+        <div class='curl1'></div>
+        <div class='curl2'></div>
+        <div class='curl3'></div>
+        <div class='curl4'></div>
+        <div class='curl5'></div>
+        <div class='curl6'></div>
+
+        <div class='body-container'>
+          <div class='body'>
+            <div class='napkin'></div>
+          </div>
+        </div>
+
+        <div class='collar'></div>
+        <div class='neck'></div>
+
+        <div class='face'>
+          <div class='parting'></div>
+          {/* <div class='curl7'></div>
+          <div class='curl8'></div> */}
+          <div class='left-ear'></div>
+          <div class='right-ear'></div>
+          <div class='left-glass'></div>
+          <div class='right-glass'></div>
+          <div class='bridge'></div>
+          <div class='glass-handle-left'></div>
+          <div class='glass-handle-right'></div>
+          <div class='left-eye'>
+            <div class='eye-left'></div>
+          </div>
+          <div class='right-eye'>
+            <div class='eye-right'></div>
+          </div>
+          <div class='nose'></div>
+          <div class='mouth'>
+            <div class='teeth'></div>
+            <div class='tongue'></div>
+          </div>
+        </div>
+      </div>
   );
 }
 
